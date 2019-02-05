d2e36be7ff4b1cc29ebe863d3e0b652be0d4fc95
Slbuck <45320541+Slbuck@users.noreply.github.com>
2019-02-05 08:55:34 -0500

    Add files via upload

--- 1.4.7.py
+++ 1.4.7.py
@@ -72,31 +72,3 @@ def frame_all_images(thickness, color, directory=None):
         framed_image.save(framed_image_filename)
     
 
-def resize_canvas(old_image_path='kimye.png', new_image_path='star.png',
-                  canvas_width=500, canvas_height=500):
-    """
-    Place one image on another image.
-
-    Resize the canvas of old_image_path and store the new image in
-    new_image_path. Center the image on the new canvas.
-    """
-    im = Image.open(old_image_path)
-    old_width, old_height = im.size
-
-    # Center the image
-    x1 = int(math.floor((canvas_width - old_width) / 2))
-    y1 = int(math.floor((canvas_height - old_height) / 2))
-
-    mode = im.mode
-    if len(mode) == 1:  # L, 1
-        new_background = (255)
-    if len(mode) == 3:  # RGB
-        new_background = (255, 255, 255)
-    if len(mode) == 4:  # RGBA, CMYK
-        new_background = (255, 255, 255, 255)
-
-    newImage = Image.new(mode, (canvas_width, canvas_height), new_background)
-    newImage.paste(im, (x1, y1, x1 + old_width, y1 + old_height))
-    newImage.save(new_image_path)
-
-resize_canvas()
